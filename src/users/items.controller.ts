a6693d777f9e72751ba98c8110339ce14fc4c9e4
elazar <tkgzreuckr@gmail.com>
2026-01-15 12:49:42 +0200

    BAD

--- src/users/items.controller.ts
+++ src/users/items.controller.ts
@@ -1,34 +1,31 @@
+import { FileInterceptor } from '@nestjs/platform-express';
 import { DtoItem } from './dto';
 import { ItemsService } from './items.service';
 import {
-    Body,
-    Controller,
-    Get,
-    HttpCode,
-    HttpStatus,
-    Post,
-    Request
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 
 @Controller('items')
 export class ItemsController {
-  constructor (
-    private itemService : ItemsService
-  ){}
+  constructor(private itemService: ItemsService) {}
 
- @HttpCode(HttpStatus.OK)
-    @Post('transactions/purchse')
-    async createItem(@Body() signInDto: DtoItem) {
-        return await this.itemService.createOne(signInDto);
-    }
-    
+  @HttpCode(HttpStatus.OK)
+  @Post('transactions/purchse')
+  async createItem(@Body() signInDto: DtoItem) {
+    return await this.itemService.createOne(signInDto);
+  }
 
-  // @Get()
-  // async getAll() {
-  //   return await this.itemService.findAll()
-  // }
-  @Get()
-  async findOne(name:string) {
-    return await this.itemService.findOne(name)
+  @Post('images/check/:itemId')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
   }
 }
